d31c2780db63070d7d213067cea5009d34c4ca1d
Alex Algarate <alex.algarate@gmail.com>
2026-04-21 15:16:24 +0200

    feat:create infra services

---
+++ apps/api/src/infrastructure/services/environment-service.ts
@@ -0,0 +1,60 @@
+import dotenv, { config } from 'dotenv';
+import * as z from 'zod';
+
+const environmentVariablesValidator = z.object({
+  ENVIRONMENT: z.enum(['local', 'production']),
+  API_PORT: z.string(),
+  JWT_SECRET: z.string(),
+  SENTRY_DSN: z.url(),
+  CORS_ORIGIN: z.string().optional(),
+  DATABASE_URL: z.string(),
+});
+
+type EnvironmentVariables = z.infer<typeof environmentVariablesValidator>;
+
+export class EnvironmentService {
+  private environmentVariables: EnvironmentVariables | null = null;
+
+  load(): void {
+    if (this.environmentVariables) return;
+
+    const currentEnvironment = process.env.NODE_ENV ?? '';
+    const environmentFile = this.getEnvironmentFile(currentEnvironment);
+
+    const variables: dotenv.DotenvConfigOutput = config({
+      path: environmentFile,
+      quiet: true,
+    });
+
+    if (variables.error) {
+      throw new Error(`Error reading environment variables: ${variables.error.message}`);
+    }
+
+    try {
+      this.environmentVariables = environmentVariablesValidator.parse({
+        ...variables.parsed,
+        ...process.env,
+      });
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? `Error validating environment variables ${error.message}` : '';
+      throw new Error(`Error validating environment variables: ${errorMessage}`);
+    }
+  }
+
+  get(): EnvironmentVariables {
+    if (!this.environmentVariables) {
+      throw new Error('Environment variables are not loaded. Use load method first');
+    }
+    return this.environmentVariables;
+  }
+
+  private getEnvironmentFile(currentEnvironment: string): '.env.production' | '.env' {
+    switch (currentEnvironment) {
+      case 'production':
+        return '.env.production';
+      default:
+        return '.env';
+    }
+  }
+}
---
+++ apps/api/src/infrastructure/services/jwt-token-service.ts
@@ -0,0 +1,33 @@
+import { TokenPayload, TokenService } from '@domain/services/TokenService';
+import jwt from 'jsonwebtoken';
+
+interface EnvironmentService {
+  get(): {
+    JWT_SECRET: string;
+  };
+}
+
+export class JwtTokenService implements TokenService {
+  private readonly jwtSecret: string;
+
+  constructor(private readonly environmentService: EnvironmentService) {
+    this.jwtSecret = this.environmentService.get().JWT_SECRET;
+  }
+
+  generate(payload: TokenPayload): string {
+    return jwt.sign(payload, this.jwtSecret, {
+      expiresIn: '1d',
+      algorithm: 'HS256',
+    });
+  }
+
+  verify(token: string): TokenPayload | null {
+    try {
+      return jwt.verify(token, this.jwtSecret, {
+        algorithms: ['HS256'],
+      }) as TokenPayload;
+    } catch (error) {
+      return null;
+    }
+  }
+}
