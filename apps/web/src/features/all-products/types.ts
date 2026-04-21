04813aa7eb82247c516c9d453912de083d654041
Alex Algarate <alex.algarate@gmail.com>
2026-04-21 18:32:00 +0200

    feat: remove all unnecessary code from /web

--- apps/web/src/features/all-products/types.ts
+++
@@ -1,8 +0,0 @@
-import { ProductDTO } from '@/domain/products/types';
-
-export interface ProductsResultDto {
-  items: ProductDTO[];
-  totalCount: number;
-  totalPages: number;
-  currentPage: number;
-}
