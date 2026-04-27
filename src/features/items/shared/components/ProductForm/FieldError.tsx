d72ace22ac5e4a06aeafa450825a789b9fbe9efc
Alex Algarate <alex.algarate@gmail.com>
2026-04-27 11:23:17 +0200

    refactor Upload form: image field

--- src/features/items/shared/components/ProductForm/FieldError.tsx
+++ src/features/items/shared/components/ProductForm/FieldError.tsx
@@ -5,5 +5,5 @@ interface FieldErrorProps {
 export const FieldError = ({ error }: FieldErrorProps) => {
   if (!error) return null;
 
-  return <p className="text-xs md:text-sm text-red-500 dark:text-red-400">{error}</p>;
+  return <p className="text-xs md:text-sm text-red-500">{error}</p>;
 };
--- src/features/items/upload/validation.ts
+++ src/features/items/upload/validation.ts
@@ -3,16 +3,16 @@ import z from 'zod';
 export const createProductSchema = z.object({
   title: z
     .string()
-    .min(1, 'Title is required')
+    .min(3, 'Title is too short, try again')
     .max(30, 'Title is too long, max 30 characters'),
   description: z
     .string()
-    .min(1, 'Description is required')
+    .min(10, 'Description is too short, try again')
     .max(200, 'Description is too long, max 200 characters'),
   price: z.number().min(1, 'Price must be positive'),
   categoryId: z.string(),
   location: z
     .string()
-    .min(1, 'Location is required')
+    .min(3, 'Location is too short, try again')
     .max(20, 'Location is too long, max 20 characters'),
 });
