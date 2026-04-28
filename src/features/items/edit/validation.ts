b5f2e17c8c39389900caaa208e4aee8c19e6c2f0
Alex Algarate <alex.algarate@gmail.com>
2026-04-28 10:52:02 +0200

    refacotr: consistency in validations messages

--- src/features/items/edit/validation.ts
+++ src/features/items/edit/validation.ts
@@ -4,18 +4,18 @@ export const updateProductSchema = z.object({
   productId: z.string().min(1, 'Product ID is required'),
   title: z
     .string()
-    .min(3, 'Title is required')
+    .min(3, 'Title is too short, try again')
     .max(30, 'Title is too long, max 30 characters'),
   description: z
     .string()
-    .min(10, 'Description nust be at least 10 characters')
+    .min(10, 'Description must be at least 10 characters')
     .max(200, 'Description is too long, max 200 characters'),
   price: z.number().min(1, 'Price must be positive'),
   categoryId: z.string().min(1, 'Category is required'),
   location: z
     .string()
-    .min(1, 'Location is required')
-    .max(20, 'Location is too long, max 20 characters'),
+    .min(3, 'Location is too short, try again')
+    .max(30, 'Location is too long, max 30 characters'),
   imageUrl: z.string().optional(),
   status: z.string().optional(),
 });
